be232194908d876c206a7b90b2f955b43a665d6c
prashant-sb <prashant.sb01@gmail.com>
2020-09-20 19:52:00 +0530

    CLI to interate and calculate file checksums from destination directory

---
+++ file_signatures/checksum.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/hex"
+	"hash/crc32"
+	"io"
+	"os"
+)
+
+const polynomial = 0xedb88320
+
+func FileMd5Sum(filePath string) (string, error) {
+	var md5sum string
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return md5sum, err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return md5sum, err
+	}
+
+	hashInBytes := hash.Sum(nil)[:16]
+	md5sum = hex.EncodeToString(hashInBytes)
+
+	return md5sum, nil
+}
+
+func FileSha256(filePath string) (string, error) {
+	var shaCheckSum string
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return shaCheckSum, err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return shaCheckSum, err
+	}
+	hashInBytes := hash.Sum(nil)[:32]
+	shaCheckSum = hex.EncodeToString(hashInBytes)
+
+	return shaCheckSum, nil
+}
+
+func FileCrc32(filePath string) (string, error) {
+	var crcCheckSum string
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return crcCheckSum, err
+	}
+	defer file.Close()
+
+	tablePolynomial := crc32.MakeTable(polynomial)
+	hash := crc32.New(tablePolynomial)
+	if _, err := io.Copy(hash, file); err != nil {
+		return crcCheckSum, err
+	}
+
+	hashInBytes := hash.Sum(nil)[:]
+	crcCheckSum = hex.EncodeToString(hashInBytes)
+
+	return crcCheckSum, nil
+}
