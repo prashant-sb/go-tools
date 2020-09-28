72646dbe2a3a2c04a791bd28581d3c2d74072d34
prashant-sb <prashant.sb01@gmail.com>
2020-09-28 12:03:01 +0530

    Work In progress : Adding netlink socket implementation: partial

--- proc_eventd/event_handler.go
+++ proc_eventd/event_handler.go
@@ -1,4 +1,8 @@
 package main
+import (
+	"golang.org/x/sys/unix"
+	log "github.com/golang/glog"
+)
 
 type Events struct {
 }
@@ -17,6 +21,15 @@ func NewEventHandler() (EventHandler, error) {
 }
 
 func (e *Events) Notify(pid uint64) error {
+
+	nlSock, err := unix.Socket(unix.AF_NETLINK, unix.SOCK_DGRAM, unix.AF_INET)
+	if err != nil {
+		log.Error("Error on creating the socket: %v", err)
+		return err
+	}
+	log.Info("Socket opened .. ")
+
+	unix.Close(nlSock)
 
 	return nil
 }
