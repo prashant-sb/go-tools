693ac058719be94c8e2372616b644e8fc018df1f
prashant-sb <prashant.sb01@gmail.com>
2020-10-26 22:42:02 +0530

    Minor changes in test

--- proc_eventd/test/proc_test.go
+++ proc_eventd/test/proc_test.go
@@ -29,6 +29,11 @@ func TestList(t *testing.T) {
 
 	pout := strings.TrimSpace(string(stdout))
 	tprocs, err := strconv.Atoi(pout)
+    if err != nil {
+		t.Errorf("List() FAILED: error %v", err.Error())
+        return
+    }
+
 	if len(pmap)+2 != tprocs {
 		t.Errorf("List() FAILED: Process count error %v:%v", tprocs, len(pmap)+2)
 		return
